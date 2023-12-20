18b4e7f0759148cb3b86da58c60f51686a5d3cde
Ronald Arifin <ronaldarifin>
2023-12-20 13:23:02 -0800

    python takes in an iterable

---
+++ General/Built Ins/join.py
@@ -0,0 +1,14 @@
+# This is how the join function works:
+
+sep = " "
+sep2 = " cool "
+
+lst = ["hello", "world", "this", "is", "a", "list"]
+print(sep.join(lst))
+print(sep2.join(lst))
+
+# Output:
+# hello world this is a list
+# hello cool world cool this cool is cool a cool list
+
+# Input: Join takes in an iterable!
