66678652b3874f81146b4d9aac84961aa505c4f3
Ronald Arifin <ronaldarifin@berkeley.edu>
2024-02-23 14:11:42 -0800

    list questions

---
+++ CS61A/Sequences/practice_list.py
@@ -0,0 +1,6 @@
+L = [1,2,3]
+B = L
+A = L[1:3]
+L[0] = A
+L += A
+# https://pythontutor.com/visualize.html#code=L%20%3D%20%5B1,2,3%5D%0AB%20%3D%20L%0AA%20%3D%20L%5B1%3A3%5D%0AL%5B0%5D%20%3D%20A%0AL%20%2B%3D%20A&cumulative=false&heapPrimitives=nevernest&mode=edit&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
