6fdc05906c2c8a13dda0ecf53906f70da0dbc802
Ronald Arifin <ronaldarifin@berkeley.edu>
2024-06-20 11:59:36 -0700

    added rv examples

---
+++ CS61A/Function/return_vs_no.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    """This function takes two numbers and returns their sum."""
+    result = a + b
+    return result
+
+def add_number_no(a, b):
+    """This function takes two numbers and prints their sum."""
+    result = a + b
+    print(result)
+
+#Example
+add_numbers(1, 2) # Evaluates to 3, printing nothing to Terminal
+add_number_no(1, 2) # Evaluates to 3, printing 3 to Terminal
